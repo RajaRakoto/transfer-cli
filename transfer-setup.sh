2027bf9d817fe31ca4f318cff82f9ce3f9b812b9
raja <raja.rakoto7@gmail.com>
2022-09-21 21:43:28 +0300

    set setup

--- transfer-setup.sh
+++ transfer-setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 echo "+++ INSTALLING DEPENDENCIES +++"
+sleep 1
 sudo wget https://raw.githubusercontent.com/gaetan1903/akory/main/akory -O /usr/local/bin/akory
 sudo chmod a+rx /usr/local/bin/akory
 echo "+++ TRANSFER-CLI INSTALLATION +++"
-sudo wget https://raw.githubusercontent.com/RajaRakoto/transfer-cli/main/transfer-cli -O /usr/local/bin/transfer-cli
+sleep 1
+sudo wget https://raw.githubusercontent.com/RajaRakoto/transfer-cli/master/transfer-cli?token=GHSAT0AAAAAABXI5LRSIRHEJTIS42I4HXTUYZLNATA -O /usr/local/bin/transfer-cli
 sudo chmod a+rx /usr/local/bin/transfer-cli
 rm transfer-setup.sh
 echo "+++ VERSION CHECKING +++"
-transfer-cli -v
+sleep 1
+transfer-cli --version
